cf9726b19d19f5be66eb4b2312b66b91468ec99a
Theara Seng <seng.theara.kh@gmail.com>
2020-04-01 19:18:42 +0700

    map static jsp to /soap/api disable datasource and other related beans

---
+++ src/main/java/com/theara/spring/controller/soap/SoapController.java
@@ -0,0 +1,30 @@
+package com.theara.spring.controller.soap;
+
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Theara Seng
+ * created on Apr 01, 2019
+ */
+
+@Controller
+public class SoapController {
+
+    @RequestMapping(value ="/", method = RequestMethod.GET)
+    public String whatever() {
+        return "index.jsp";
+    }
+
+    @RequestMapping(value ="/soap/api", method = RequestMethod.GET)
+    public String test() {
+        return "success-response.jsp";
+    }
+
+}
