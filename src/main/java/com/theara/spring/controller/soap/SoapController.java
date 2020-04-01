dc176b9e49ea63b32f3fd785bbd7f8c231265a0c
Theara Seng <seng.theara.kh@gmail.com>
2020-04-01 19:20:14 +0700

    remove unnecessary import packages

--- src/main/java/com/theara/spring/controller/soap/SoapController.java
+++ src/main/java/com/theara/spring/controller/soap/SoapController.java
@@ -1,13 +1,8 @@
 package com.theara.spring.controller.soap;
 
-import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Theara Seng
