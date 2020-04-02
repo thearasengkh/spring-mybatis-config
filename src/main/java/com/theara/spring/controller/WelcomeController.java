547571c443cdf6b48a1500f91c0514d7d5178310
Theara Seng <seng.theara.kh@gmail.com>
2020-04-02 23:12:01 +0700

    example of org.springframework.web.servlet.handler.SimpleUrlHandlerMapping

---
+++ src/main/java/com/theara/spring/controller/WelcomeController.java
@@ -0,0 +1,25 @@
+package com.theara.spring.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Theara Seng
+ * created on Apr 02, 2020
+ */
+
+public class WelcomeController extends AbstractController {
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+
+    }
+
+}
