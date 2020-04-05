dbcfe95bdc01b609b19b35293ad4f06f5bba3fff
Theara Seng <seng.theara.kh@gmail.com>
2020-04-05 16:45:11 +0700

    config Content Negotiation using suffix strategy [xml config]

--- src/main/java/com/theara/spring/controller/WelcomeController.java
+++ src/main/java/com/theara/spring/controller/WelcomeController.java
@@ -1,8 +1,12 @@
 package com.theara.spring.controller;
 
 import com.theara.spring.model.MData;
+import com.theara.spring.model.cbc.ENQUIRY;
+import com.theara.spring.model.cbc.MESSAGE;
+import com.theara.spring.model.cbc.REQUEST;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +25,16 @@ public class WelcomeController {
     @Autowired
     @Qualifier("beanFromRootContext")
     private MData beanFromRootContext;
+
+    @RequestMapping(
+            value = "/content-negotiation-json",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            method = RequestMethod.GET
+    )
+    @ResponseBody
+    public REQUEST contentNegotiationJson() {
+        return makeDummyXmlData();
+    }
 
     @RequestMapping(value = "/response-body", method = RequestMethod.GET)
     @ResponseBody
@@ -63,6 +77,18 @@ public class WelcomeController {
         jsonObj.setString("name", "DARA SOMNANG");
         jsonObj.setString("job","Software Engineer");
         return jsonObj;
+    }
+
+    private REQUEST makeDummyXmlData() {
+
+        REQUEST request = new REQUEST();
+        MESSAGE message = new MESSAGE();
+        ENQUIRY enquiry = new ENQUIRY();
+        enquiry.setENQUIRY_REFERENCE("XXX123456789XXX");
+        message.setENQUIRY(enquiry);
+        request.setMESSAGE(message);
+        return request;
+
     }
 
 }
